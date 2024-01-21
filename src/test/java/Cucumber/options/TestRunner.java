8836dfde3e8877a3b0a1208d7cb67f835e61a36d
HarshalHarawat <Harshaljain830@gmail.com>
2024-01-21 15:36:13 +0530

    Changes done y

--- src/test/java/Cucumber/options/TestRunner.java
+++ src/test/java/Cucumber/options/TestRunner.java
@@ -9,8 +9,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/Feature",
-		glue= {"stepDefination"}
-	//	tags= {"@Smoke"}
+		glue= {"stepDefination"},
+		tags= {"@Smoke"}
 	//	tags= {"@Smoke and @Regression"}
 	//	tags= {"@Smoke or @Regression"}
 	//	tags= {"@Smoke","@Regression"},
@@ -21,5 +21,6 @@ import io.cucumber.junit.CucumberOptions;
 		
 		)
 public class TestRunner {
+	
 
 }
